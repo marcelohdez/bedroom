7aee7e43f81d2f9deeda37e2dc99e029a62ad330
swiftsatchel <76508651+swiftsatchel@users.noreply.github.com>
2021-08-12 00:34:11 -0400

    WindowParent documentation

--- src/main/java/com/swiftsatchel/bedroom/util/WindowParent.java
+++ src/main/java/com/swiftsatchel/bedroom/util/WindowParent.java
@@ -1,8 +1,23 @@
 package com.swiftsatchel.bedroom.util;
 
+/**
+ * Allows this window's children to have some information and control over them.
+ */
 public interface WindowParent {
 
+    /**
+     * Used to return this window's x, y, width, and height respectively in an int[]
+     * to be used for centering, etc. by the child
+     *
+     * @return An int[] of the window's x, y, width, and height
+     */
     int[] getXYWidthHeight();
+
+    /**
+     * Used to show/hide this window by the child.
+     *
+     * @param b Should this window be visible?
+     */
     void makeVisible(boolean b);
 
 }
