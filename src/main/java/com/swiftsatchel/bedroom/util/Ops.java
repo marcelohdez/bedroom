86ede3ac11e3f3718a76a555b03d226ab065df5e
swiftsatchel <76508651+swiftsatchel@users.noreply.github.com>
2021-09-06 00:29:41 -0400

    Optimized Ops methods

--- src/main/java/com/swiftsatchel/bedroom/util/Ops.java
+++ src/main/java/com/swiftsatchel/bedroom/util/Ops.java
@@ -2,32 +2,46 @@ package com.swiftsatchel.bedroom.util;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
 
 public final class Ops { // Operations
 
     /**
-     * Creates a String[] of numbers with optional added text to each.
+     * Creates a String[] of numbers with added text to each.
      *
      * @param start Starting number
      * @param end Ending number
-     * @param extraText Optional added text, can be null
+     * @param extraText Optional added text
      * @return A String[] of numbers with optional added text to each.
      */
     public static String[] createNumberList(boolean addZeroUnder10, int start, int end, String extraText) {
 
-        ArrayList<String> list = new ArrayList<>();
+        String[] list = new String[(end+1)-start];
         StringBuilder sb;
-        for (int i = start; i <= end; i++) {
+        for (int i = 0; i < list.length; i++) {
             sb = new StringBuilder();
-            sb.append(i < 10 && addZeroUnder10 ? "0" + i : i);
-            if (extraText != null) sb.append(extraText);
-            list.add(sb.toString());
+            sb.append(addZeroUnder10 && i < 10 ? "0" + i : i).append(extraText);
+            list[i] = sb.toString();
         }
 
-        // This line was gotten from Floern and Bozho's response on StackOverflow:
-        // https://stackoverflow.com/questions/4042434/converting-arrayliststring-to-string-in-java
-        return list.toArray(new String[0]);
+        return list;
+
+    }
+
+    /**
+     * Creates a String[] of numbers.
+     *
+     * @param start Starting number
+     * @param end Ending number
+     * @return A String[] of numbers with optional added text to each.
+     */
+    public static String[] createNumberList(boolean addZeroUnder10, int start, int end) {
+
+        String[] list = new String[(end+1)-start];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = String.valueOf(addZeroUnder10 && i < 10 ? "0" + i : i);
+        }
+
+        return list;
 
     }
 
@@ -38,8 +52,7 @@ public final class Ops { // Operations
      * @return An "s" if number is greater than 1, else ""
      */
     public static String isPlural(int number) { // Return "s" if there is more than 1 of number
-        if (number > 1) return "s";
-        return "";
+        return number > 1 ? "s" : "";
     }
 
     /**
