a37b125b27281a24b76ae0013ead7aa9dd093021
swiftsatchel <76508651+swiftsatchel@users.noreply.github.com>
2021-09-22 21:03:37 -0400

    bumped version to beta 6

--- src/main/java/com/swiftsatchel/bedroom/Main.java
+++ src/main/java/com/swiftsatchel/bedroom/Main.java
@@ -27,7 +27,7 @@ import java.util.prefs.Preferences;
 public class Main {
 
     // ======= Global Variables =======
-    public static final String VERSION = "3 (Beta 5)";
+    public static final String VERSION = "3 (Beta 6)";
     public static final Preferences userPrefs = Preferences.userRoot(); // User preferences directory
 
     // ======= Variables =======
